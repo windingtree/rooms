672989b989471c25f981918ca0339cff1af6720e
Valera Rozuvan <valera.rozuvan@gmail.com>
2020-09-14 15:07:11 +0300

    Convert to Serverless Function.

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,26 +1,6 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv'
+import { NowRequest, NowResponse } from '@vercel/node'
 
-dotenv.config();
-
-import { IPingResponse } from './interfaces';
-
-const PORT = process.env.PORT || 3000;
-
-const app = express();
-app.use(bodyParser.json());
-
-app.get('/ping', async (req, res) => {
-  const response: IPingResponse = {
-    pong: 'OK'
-  };
-
-  res.send(response);
-});
-
-(async () => {
-  app.listen(PORT, () => {
-    console.log('Started at http://localhost:' + PORT);
-  });
-})();
+export default (request: NowRequest, response: NowResponse) => {
+  const { name = 'World' } = request.query
+  response.status(200).send(`Hello ${name}!`)
+}
