c9873c2f92d8f99008397324bb903d26de666b7e
Valera Rozuvan <valera.rozuvan@gmail.com>
2020-09-18 23:52:51 +0300

    Fix prod deploy.

--- api/v1/index.ts
+++ api/v1/index.ts
@@ -1,5 +1,7 @@
 import { NowRequest, NowResponse } from '@vercel/node'
 
+const API_HOST_URL = process.env.API_HOST_URL
+
 export default (request: NowRequest, response: NowResponse) => {
-  response.redirect(307, 'https://staging.rooms.windingtree.com/api/doc')
+  response.redirect(307, `https://${API_HOST_URL}/api/doc`)
 }
