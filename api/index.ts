dca0e086a34ec40685ce112f0fa9ab075c678261
Valera Rozuvan <valera.rozuvan@gmail.com>
2020-09-18 11:55:10 +0300

    Redirect to Swagger docs for /api endpoint.

--- api/index.ts
+++ api/index.ts
@@ -1,14 +1,5 @@
 import { NowRequest, NowResponse } from '@vercel/node'
 
 export default (request: NowRequest, response: NowResponse) => {
-  response.status(200).send(
-    `
-      <p>
-        OpenAPI docs available at
-        <a href="https://staging.rooms.windingtree.com/api/doc">
-          https://staging.rooms.windingtree.com/api/doc
-        </a>
-      </p>
-    `
-  )
+  response.redirect(307, 'https://staging.rooms.windingtree.com/api/doc')
 }
