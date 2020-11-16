0c93dfe61cc1e989ccd8dd3fa05195f33d41aa0e
Valera Rozuvan <valera.rozuvan@gmail.com>
2020-11-16 14:19:06 +0200

    Initial version of working offers/search.

--- api/v1/offers/search.ts
+++ api/v1/offers/search.ts
@@ -1,13 +1,50 @@
 import { NowRequest, NowResponse } from '@vercel/node'
-import { v4 as uuidv4 } from 'uuid'
+// import { v4 as uuidv4 } from 'uuid'
 
-// import { getAllRoomTypes } from '../../_lib/data'
+import { getAllRoomTypes } from '../../_lib/data'
 import { readHotels } from '../../_lib/data/hotel'
 import { verifyOrgJwt } from '../../_lib/data/marketplace'
 import { genericApiMethodHandler, errorHandler, getOrgToken } from '../../_lib/tools'
-import { /* IVerifiedOrgJwtResults, */ /* IRoomTypeCollection, */ IHotelCollection } from '../../_lib/types'
+import { /* IVerifiedOrgJwtResults, */ IRoomTypeCollection, IHotelCollection } from '../../_lib/types'
+
+interface IAccommodationsRoomType {
+  [key:string]: unknown
+}
+
+interface IAccommodation {
+  [key:string]: unknown
+
+  roomTypes: IAccommodationsRoomType
+}
+
+interface IAccommodations {
+  [key:string]: IAccommodation
+}
+
+interface IPricePlans {
+  [key:string]: unknown
+}
+
+interface IOffers {
+  [key:string]: unknown
+}
+
+interface IPassengers {
+  [key:string]: unknown
+}
+
+interface IOfferSearchResults {
+  accommodations: IAccommodations
+  pricePlans: IPricePlans
+  offers: IOffers
+  passengers: IPassengers
+}
 
 async function POST(request: NowRequest, response: NowResponse): Promise<void> {
+  console.log('API request for offers/search')
+  console.log('BODY =>')
+  console.log(JSON.stringify(request.body))
+
   let jwt: string
   try {
     jwt = getOrgToken(request)
@@ -23,12 +60,12 @@ async function POST(request: NowRequest, response: NowResponse): Promise<void> {
     return errorHandler(response, err)
   }
 
-  // let roomTypeCollection: IRoomTypeCollection
-  // try {
-  //   roomTypeCollection = await getAllRoomTypes()
-  // } catch (err) {
-  //   return errorHandler(response, err)
-  // }
+  let roomTypeCollection: IRoomTypeCollection
+  try {
+    roomTypeCollection = await getAllRoomTypes()
+  } catch (err) {
+    return errorHandler(response, err)
+  }
 
   let hotelCollection: IHotelCollection
   try {
@@ -58,17 +95,98 @@ async function POST(request: NowRequest, response: NowResponse): Promise<void> {
   //   return el !== null
   // })
 
-  const offers = hotelCollection.map((hotel) => {
-    return {
-      id: uuidv4(),
-      type: 'small',
-      price: 20.99,
-      hotel: hotel.name,
-      address: hotel.address,
+  const result: IOfferSearchResults = {
+    accommodations: {},
+    "pricePlans": {
+      "BAR": {
+        "name": "Winding Tree BAR",
+        "penalties": {
+          "refund": {
+            "refundable": true
+          }
+        }
+      }
+    },
+    offers: {},
+    passengers: {
+      "PAX1": {
+        "type": "ADT"
+      }
     }
+  }
+
+  hotelCollection.forEach((hotel) => {
+    result.accommodations[hotel.id] = {
+      name: hotel.name,
+      type: 'hotel',
+      description: 'Hotel provided by Rooms project.',
+      location: {
+        coordinates: {
+          latitude: hotel.location.lat,
+          longitude: hotel.location.lng,
+        },
+      },
+      rating: 5,
+      contactInformation: {
+        phoneNumbers: [],
+        emails: [],
+        address: {
+          streetAddress: hotel.address,
+          premise: '',
+          locality: '',
+          postalCode: '',
+          country: '',
+        },
+      },
+      checkinoutPolicy: {
+        checkinTime: "15:00",
+        checkoutTime: "24:00",
+      },
+      otherPolicies: {},
+      media: [],
+      roomTypes: {},
+    }
+
+    roomTypeCollection.forEach((roomType) => {
+      result.accommodations[hotel.id].roomTypes[roomType.id] = {
+        "name": roomType.type,
+        "description": 'Room provided by Rooms project.',
+        "amenities": roomType.amenities.split(';'),
+        "size": {
+          "value": "",
+          "_unit_": ""
+        },
+        "maximumOccupancy": {
+          "adults": "2",
+          "childs": "1"
+        },
+        "media": [],
+        "policies": {}
+      }
+    })
   })
 
-  response.status(200).json({ offers })
+  roomTypeCollection.forEach((roomType) => {
+    hotelCollection.forEach((hotel) => {
+
+      result.offers[`${roomType.id}-${hotel.id}`] = {
+        pricePlansReferences: {
+          BAR: {
+            accommodation: hotel.id,
+            roomType: roomType.type,
+          },
+        },
+        price: {
+          currency: 'EUR',
+          public: roomType.price,
+          taxes: 0,
+        },
+      }
+
+    })
+  })
+
+  response.status(200).json(result)
 }
 
 export default async (request: NowRequest, response: NowResponse): Promise<void> => {
