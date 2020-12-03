4214901f5a28d2ae5060e151019a006b6327d4c2
Valera Rozuvan <valera.rozuvan@gmail.com>
2020-12-03 18:12:38 +0200

    [be] Allow non numeric values for search rect. (#25)

--- api/_lib/types/geo.ts
+++ api/_lib/types/geo.ts
@@ -1,4 +1,11 @@
 interface ILocationRectangle {
+  north: number|string|null|undefined
+  south: number|string|null|undefined
+  west: number|string|null|undefined
+  east: number|string|null|undefined
+}
+
+interface ILocationRectangleDbType {
   north: number
   south: number
   west: number
@@ -7,4 +14,5 @@ interface ILocationRectangle {
 
 export {
   ILocationRectangle,
+  ILocationRectangleDbType,
 }
