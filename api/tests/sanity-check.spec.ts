122f0aec5cba2f3e65eef504b19dc72e1ab7133c
Valera Rozuvan <valera.rozuvan@gmail.com>
2020-12-14 14:03:15 +0200

    [be] Add unit tests. (#53)

---
+++ api/tests/sanity-check.spec.ts
@@ -0,0 +1,10 @@
+import { hello } from './sanity-check'
+import { expect } from 'chai'
+import 'mocha'
+
+describe('Hello function', () => {
+  it('should return hello world', () => {
+    const result = hello()
+    expect(result).to.equal('Hello world!')
+  })
+})
