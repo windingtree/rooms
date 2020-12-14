b46c57c28b5e0be3e5ab51c3c7691242d144b5f9
Valera Rozuvan <valera.rozuvan@gmail.com>
2020-12-14 11:03:29 +0200

    [be] Check creditorOrgId.

--- api/_lib/app/orgid/createOrder.ts
+++ api/_lib/app/orgid/createOrder.ts
@@ -3,16 +3,24 @@ import { v4 as uuidv4 } from 'uuid'
 import { getPaymentInfo } from '../../../_lib/data/simard'
 import { readOfferByOfferId, deleteOfferByOfferId } from '../../../_lib/data/offer'
 import { createBooking } from '../../../_lib/data/booking'
+import { AppConfig } from '../../../_lib/infra/config'
 import { emailNewBooking, CError } from '../../../_lib/tools'
-import { IPostCreateOrderPayload, ICreateOrderResult, IOrgDetails, IOffer, IBaseBooking, IPaymentInfo } from '../../../_lib/types'
+import { IPostCreateOrderPayload, ICreateOrderResult, IOrgDetails, IOffer, IBaseBooking, ISimardPaymentInfo } from '../../../_lib/types'
 import { CONSTANTS } from '../../../_lib/infra/constants'
 
 const { BAD_REQUEST } = CONSTANTS.HTTP_STATUS
 
 async function createOrder(requester: IOrgDetails, payload: IPostCreateOrderPayload): Promise<ICreateOrderResult> {
-  const paymentInfo: IPaymentInfo = await getPaymentInfo(payload.guaranteeId)
-  if (paymentInfo.status !== 'OK') {
-    throw new CError(BAD_REQUEST, 'Must provcide a valid "guaranteeId" to make an order.')
+  const appConfig = await AppConfig.getInstance().getConfig()
+
+  const paymentInfo: ISimardPaymentInfo = await getPaymentInfo(payload.guaranteeId)
+
+  console.log('--------------')
+  console.log(paymentInfo)
+  console.log('--------------')
+
+  if (paymentInfo.creditorOrgId !== appConfig.WT_ROOMS_ORGID) {
+    throw new CError(BAD_REQUEST, 'Guarantee not meant for Rooms organization.')
   }
 
   const offer: IOffer = await readOfferByOfferId(payload.offerId)
