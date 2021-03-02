ad9d5b0ede1919c245c870d6df0103fa78e7bc63
Valera Rozuvan <2c7v7czk85j3@protonmail.com>
2021-03-02 15:02:53 +0200

    [fix] Proper S3 integration. (#96)

--- api/_lib/app/upload_image/uploadImageToS3.ts
+++ api/_lib/app/upload_image/uploadImageToS3.ts
@@ -10,6 +10,31 @@ import { IProfile, IPostUploadImagePayload, IUploadImage } from '../../common/ty
 
 const { BAD_GATEWAY } = CONSTANTS.HTTP_STATUS
 
+function uploadToS3Handler(s3bucket: AWS.S3, params: AWS.S3.PutObjectRequest): Promise<string> {
+  let resolve: (fileUrl: string) => void
+  let reject: (err: unknown) => void
+
+  const promise = new Promise<string>((_resolve, _reject) => {
+    resolve = _resolve
+    reject = _reject
+  })
+
+  s3bucket.upload(params, async (err: unknown, data: AWS.S3.ManagedUpload.SendData) => {
+    if (err) {
+      console.log('s3bucket.upload() error:')
+      console.log(err)
+
+      reject(err)
+    }
+
+    console.log(`File uploaded successfully. S3 file is located at: '${data.Location}'.`);
+
+    resolve(data.Location)
+  });
+
+  return promise
+}
+
 async function uploadImageToS3(requester: IProfile, payload: IPostUploadImagePayload): Promise<IUploadImage> {
   const imageFile = await Jimp.read(Buffer.from(payload.file, 'base64'))
   await imageFile.resize(Jimp.AUTO, 600)
@@ -23,24 +48,21 @@ async function uploadImageToS3(requester: IProfile, payload: IPostUploadImagePay
   })
 
   const s3FileHash = uuidv4()
-  const s3FileURL = `https://s3-${appConfig.AWS_REGION}.amazonaws.com/${appConfig.AWS_BUCKET_NAME}/${s3FileHash}`
 
   const params = {
     Bucket: appConfig.AWS_BUCKET_NAME,
     Key: s3FileHash,
-    Body: imageFile,
+    Body: await imageFile.getBufferAsync(imageFile.getMIME()),
     ContentType: imageFile.getMIME(),
     ACL: 'public-read'
   }
 
-  console.log('PARAMS', params);
+  console.log('PARAMS')
+  console.log(params)
 
+  let s3FileURL = ''
   try {
-    await s3bucket.upload(params, async (err: unknown) => {
-      if (err) {
-        throw new CError(BAD_GATEWAY, `Something bad happened while uploading image to S3.`, err)
-      }
-    });
+    s3FileURL = await uploadToS3Handler(s3bucket, params)
   } catch (err: unknown) {
     throw new CError(BAD_GATEWAY, `Could not upload image to S3.`, err)
   }
