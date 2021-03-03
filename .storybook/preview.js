85b4c022b28c4e6522f8530fcd807710f1c771cd
kurt <tomashq@gmail.com>
2021-03-03 13:07:34 +0100

    storybook previews to use same CSS/fonts as the rest of app

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,18 @@
+import React from 'react';
+
+import {ThemeProvider} from '@material-ui/core/styles'
+import {roomsTheme} from '../src/utils/themes/index'
+
+import '../src/index.css'
+
+export const decorators = [
+    (story) => (
+        <ThemeProvider theme={roomsTheme}>
+            {story()}
+        </ThemeProvider>
+    ),
+];
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: {argTypesRegex: "^on[A-Z].*"},
 }
